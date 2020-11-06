602b6e48ce4dad78587cfac43a898eff689c160e
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-11-06 10:23:09 -0800

    add has_many answers to question model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_06_175451) do
+ActiveRecord::Schema.define(version: 2020_11_06_181520) do
+
+  create_table "answers", force: :cascade do |t|
+    t.boolean "is_correct?"
+    t.string "content"
+    t.integer "question_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "content"
